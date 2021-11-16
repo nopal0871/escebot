993722c54217afbc98603eb552f69290054962b7
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-16 12:05:33 +0700

    Update caliph.js

--- message/caliph.js
+++ message/caliph.js
@@ -23,6 +23,8 @@ let bdr = require("rumus-bdr")
 let uploadFile = require("../lib/uploadFile")
 let ff = require('fluent-ffmpeg') 
 let tebakkata = {}
+let moment = require('moment')
+let mtz = require('moment-timezone')
 let {
 MessageType: mType,
 GroupSettingChange: gcSet
