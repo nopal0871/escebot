3167102669f49bb8f47c15b1e75336d3488a769d
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-12-11 05:37:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 let express = require('express')
 let path = require('path')
 let qrcode = require('qrcode')
-let ngrok = require('ngrok')
 let uploadFile = require('./lib/uploadFile')
 let fs = require('fs')
 
-async function connect(conn, PORT) {
+async function connect(conn, PORT, use_ngrok = false) {
     let app = global.app = express()
     let _qr = 'invalid'
+    if (use_ngrok) {
+        let ngrok = require('ngrok')
       const ngrok_url = await ngrok.connect({
   proto: 'http', 
   addr: PORT, 
   region: 'jp'
    })
+       console.log('Scan QR on :', ngrok_url)
+      }
     app.use(async (req, res) => {
         if (req.path == '/session' && conn.state == 'open') return res.send(conn.base64EncodedAuthInfo())
         if (conn.state == 'open') return res.status(200).send({status: 200, message: 'Bot Telah Tersambung ke whatsapp web anda!', user: conn.user })
@@ -21,7 +24,7 @@ async function connect(conn, PORT) {
         html = fs.readFileSync('./views/scan.html', 'utf-8')
         res.send(html.replace(/\$QRURL/g, url))
     })
-    console.log('Scan QR on :', ngrok_url)
+   
     conn.on('qr', qr => {
         _qr = qr
     })
